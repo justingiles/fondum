92eb28da1ef10489627574d0d90b91744db5a489
John Dupuy <john@cattailcreek9.com>
2017-11-01 22:45:11 -0500

    another attempt at path fix

--- fondum/cli.py
+++ fondum/cli.py
@@ -19,10 +19,10 @@ import subprocess
 from distutils import dir_util
 import sys
 
-# Insert project root path to sys.path
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+project_path = os.path.abspath(os.path.dirname(__file__))
 if project_path not in sys.path:
     sys.path.insert(0, project_path)
+print("project_path", project_path)
 
 import supercopy
 from supercopy import SELF_NAME, NOW
